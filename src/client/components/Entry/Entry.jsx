e0e42abfdd73f0a4ef0cfc4aa4acabd38a0494e1
Heather <havmusson@uwaterloo.ca>
2020-07-09 15:13:57 -0400

    Add more work entries

--- src/client/components/Entry/Entry.jsx
+++ src/client/components/Entry/Entry.jsx
@@ -6,24 +6,35 @@ import style from './Entry.less';
 
 import { FaAngleRight } from 'react-icons/fa';
 
-const Hero = ({link, isExternal, type, image, title, description, btn}) => {
-  const fLink = isExternal ? <a href={link}>{btn}<FaAngleRight /></a> : <Link to={'/' + link}>{btn}<FaAngleRight /></Link>;
+const FLink =({showLink, link, isExternal, btn}) => {
+  if (!showLink) {
+    return (<p className={style.btn}>{btn}</p>);
+  } else if(isExternal) {
+    return (<p className={style.btn}><a href={link} target="_blank">{btn}<FaAngleRight /></a></p>);
+  } else {
+    return (<p className={style.btn}><Link to={'/' + link}>{btn}<FaAngleRight /></Link></p>);
+  }
+}
 
-  return (
-    <div className={style.entry}>
-      <img href={link} className={style.entryImg} src={image}/>
-      <div className={style.entryText}>
-          <span className={style.type}>{type}</span>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <div class="meta-wrap align-items-center">
-              <div class="half">
-                  <p className={style.btn}>{fLink}</p>
-              </div>
-          </div>
-      </div>
+const Hero = ({showLink = true, link, isExternal, type, image, title, description, btn}) => (
+  <div className={style.entry}>
+    <img href={link} className={style.entryImg} src={image}/>
+    <div className={style.entryText}>
+        <span className={style.type}>{type}</span>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <div class="meta-wrap align-items-center">
+            <div class="half">
+              <FLink
+                showLink={showLink}
+                link={link}
+                isExternal={isExternal}
+                btn={btn}
+              />
+            </div>
+        </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Hero;
--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -9,106 +9,145 @@ import Entry from '../components/Entry/Entry.jsx';
 
 import Background from '../../../public/images/background.jpg';
 import Author from '../../../public/images/author.jpg';
+import Heather from '../../../public/images/heather.png';
 import Shopify from '../../../public/images/shopify.png';
 import Forest from '../../../public/images/forest.png';
 import Housing from "../../../public/images/housing.jpg";
 import Smile from "../../../public/images/smile.png";
 import Foodwaste from "../../../public/images/foodwaste.jpg";
+import Brisk from '../../../public/images/brisk.png';
+import PharmAchieve from '../../../public/images/pharmachieve.png';
 import Connect5 from "../../../public/images/connect5.jpg";
 
 import { FaAngleRight } from 'react-icons/fa';
 
 class HomePage extends Component {
-    render() {
-        return (
-            <div className={style.page}>
-                <SideBar />
-                <div className={style.main}>
-                    <Hero background={Background}>
-                        <img src={Author} className={style.authorImg}/>
-                        <div>
-                            <h2 className={style.subheading}>Hello I'm</h2>
-                            <h1 className={style.heading}>Heather Musson</h1>
-                            <p>I'm a third year software engineering student at the University of Waterloo, with an interest in web development and design, and experience working with a wide variety of technologies.</p>
-                            <p><Link className={style.btnCustom} to='/about'>More About Me <FaAngleRight /></Link></p>
-                        </div>
-                    </Hero>
-                    <section className={style.section}>
-                        <div class="container">
-                            <div className={style.row}>
-                                <div className={style.col4}>
-                                    <Entry  
-                                        link="https://devpost.com/software/forest-zfr4ye" 
-                                        isExternal={true}
-                                        image={Forest}
-                                        type="Project" 
-                                        title="Forest." 
-                                        description="Contact tracing in hospitals is undeniably lack, with nurses often writing tracking on pieces of paper - we aim to fix this." 
-                                        btn="More"
-                                    />
-                                </div>
-                                <div className={style.col4}>
-                                    <Entry  
-                                        link="https://www.shopify.com/" 
-                                        isExternal={true}
-                                        image={Shopify}
-                                        type="Work" 
-                                        title="Shopify" 
-                                        description="Web Developer" 
-                                        btn="In progress"
-                                    />
-                                </div>
-                                <div className={style.col4}>
-                                    <Entry  
-                                        link="https://github.com/heathermusson/UWHousing" 
-                                        isExternal={true}
-                                        image={Housing}
-                                        type="Project" 
-                                        title="UWHousing" 
-                                        description="A centralized system that helps students find short-term housing and post their own listings." 
-                                        btn="In progress"
-                                    />
-                                </div>
-                                <div className={style.col4}>
-                                    <Entry  
-                                        link="smile" 
-                                        isExternal={false}
-                                        image={Smile}
-                                        type="Work" 
-                                        title="Smile.io" 
-                                        description="Full Stack Web Developer" 
-                                        btn="More"
-                                    />
-                                </div>
-                                <div className={style.col4}>
-                                    <Entry 
-                                        link="foodwaste"
-                                        isExternal={false}
-                                        image={Foodwaste}
-                                        type="Design"
-                                        title="Food Waste Experience"
-                                        description="This mobile app concept imagines a way to help reduce food waste through a social media platform."
-                                        btn="More"
-                                    />
-                                </div>
-                                <div className={style.col4}>
-                                    <Entry 
-                                        link="https://github.com/heathermusson/First-Year-Design-Project"
-                                        isExternal={true}
-                                        image={Connect5}
-                                        type="Project"
-                                        title="Connect 5"
-                                        description="An Arduino-based Connect 5 game featuring an intelligent computer player."
-                                        btn="More"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
+  render() {
+    return (
+      <div className={style.page}>
+        <SideBar />
+        <div className={style.main}>
+          <Hero background={Background}>
+            <img src={Author} className={style.authorImg}/>
+            <div>
+                <h2 className={style.subheading}>Hello I'm</h2>
+                <h1 className={style.heading}>Heather Musson</h1>
+                <p>I'm a third year software engineering student at the University of Waterloo, with an interest in web development and design, and experience working with a wide variety of technologies.</p>
+                <p><Link className={style.btnCustom} to='/about'>More About Me <FaAngleRight /></Link></p>
             </div>
-        );
-    };
+          </Hero>
+          <section className={style.section}>
+            <div class="container">
+              <div className={style.row}>
+                <div className={style.col4}>
+                  <Entry
+                    link="https://github.com/heathermusson/heather-react"
+                    isExternal={true}
+                    image={Heather}
+                    type="Project"
+                    title="Personal Website"
+                    description="Have a look at the code used to create this ReactJS website."
+                    btn="More"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="https://devpost.com/software/forest-zfr4ye"
+                    isExternal={true}
+                    image={Forest}
+                    type="Project"
+                    title="Forest"
+                    description="Contact tracing in hospitals is undeniably lack, with nurses often writing tracking on pieces of paper - we aim to fix this."
+                    btn="More"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="https://www.shopify.com/"
+                    showLink={false}
+                    isExternal={true}
+                    image={Shopify}
+                    type="Work"
+                    title="Shopify"
+                    description="Web Developer"
+                    btn="In progress"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="https://github.com/heathermusson/UWHousing"
+                    isExternal={true}
+                    image={Housing}
+                    type="Project"
+                    title="UWHousing"
+                    description="A centralized system that helps students find short-term housing and post their own listings."
+                    btn="In progress"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="smile"
+                    isExternal={false}
+                    image={Smile}
+                    type="Work"
+                    title="Smile.io"
+                    description="Full Stack Web Developer"
+                    btn="More"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="foodwaste"
+                    isExternal={false}
+                    image={Foodwaste}
+                    type="Design"
+                    title="Food Waste Experience"
+                    description="This mobile app concept imagines a way to help reduce food waste through a social media platform."
+                    btn="More"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="https://brisksynergies.com/"
+                    showLink={true}
+                    isExternal={true}
+                    image={Brisk}
+                    type="Work"
+                    title="Brisk Synergies"
+                    description="Full Stack Developer"
+                    btn="More"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="#"
+                    showLink={false}
+                    isExternal={true}
+                    image={PharmAchieve}
+                    type="Work"
+                    title="PharmAchieve"
+                    description="Full Stack Web Developer"
+                    btn="Completed"
+                  />
+                </div>
+                <div className={style.col4}>
+                  <Entry
+                    link="https://github.com/heathermusson/First-Year-Design-Project"
+                    isExternal={true}
+                    image={Connect5}
+                    type="Project"
+                    title="Connect 5"
+                    description="An Arduino-based Connect 5 game featuring an intelligent computer player."
+                    btn="More"
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  };
 };
 
 export default HomePage;
